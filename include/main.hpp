858377f0e2692e577cb2ee5848e50b18285dbaf0
Dean du Toit <dean100@live.co.za>
2017-07-26 16:09:01 +0200

    Moved Libs into Platform Specifics Folders. Linked Mixers, TTF, Image

--- include/main.hpp
+++ include/main.hpp
@@ -5,10 +5,12 @@
 #endif 
 
 #include <SDL.h>
+#include <SDL_ttf.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
 #include <GL/glew.h>
 #include <SDL_opengl.h>
 #include <gl\glu.h>
 #include <iostream>
-
 
 void	startGUI(SDL_Window *win);
