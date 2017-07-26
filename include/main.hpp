f40fbc604fa7af2d305f818248332fc040f67df9
Dean du Toit <dean100@live.co.za>
2017-07-26 15:26:03 +0200

    Added SDL And Glew Init. Created Basic window with a basic ass triangle

--- include/main.hpp
+++ include/main.hpp
@@ -1,1 +1,14 @@
 #pragma once
+
+#ifdef  WIN32
+# include <Windows.h>
+#endif 
+
+#include <SDL.h>
+#include <GL/glew.h>
+#include <SDL_opengl.h>
+#include <gl\glu.h>
+#include <iostream>
+
+
+void	startGUI(SDL_Window *win);
--- targsrc/main.cpp
+++ targsrc/main.cpp
@@ -9,10 +9,50 @@
 
 #include "main.hpp"
 
-#include <iostream>
-
-int	main()
+// Initialize SDL and GLEW
+SDL_Window *initWindow()
 {
-	std::cout << "From humble beginnings" << std::endl;
+	if (SDL_Init(0) == -1)
+	{
+		std::cerr << "Error : " << SDL_GetError() << std::endl;
+		return (nullptr);
+	}
+	SDL_Window *window = SDL_CreateWindow(
+		"Bomberman", 100, 100, 640, 480,
+		SDL_WINDOW_OPENGL);
+	SDL_GLContext gContext = SDL_GL_CreateContext(window);
+	if (gContext == NULL) 
+	{ 
+		std::cerr << "OpenGL context could not be created! SDL Error: %s\n" << SDL_GetError();
+		return (nullptr);
+	}
+	glewExperimental = GL_TRUE;
+	GLenum err = glewInit();
+	if (err != GLEW_OK)
+	{
+		std::cerr << "Error : " << "Failed to initialize GLEW" << std::endl;
+		return nullptr;
+	}
+	return (window);
+}
+
+int initGlew()
+{
+	glewExperimental = GL_TRUE;
+	GLenum err = glewInit();
+	if (err != GLEW_OK)
+	{
+		std::cerr << "Error : " << "Failed to initialize GLEW" << std::endl;
+		return -1;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	SDL_Window	*window = initWindow();
+	if (window == nullptr || initGlew() == -1)
+		return (-1);
+	startGUI(window);
+	SDL_Quit();
 	return (0);
 }
