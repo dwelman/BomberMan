d3f0be18cafee0ff7db943ee3cf14ac9e9034b8b
Dean du Toit <dean100@live.co.za>
2017-07-26 17:36:10 +0200

    Added glm and extra dll copies

--- include/main.hpp
+++ include/main.hpp
@@ -4,13 +4,18 @@
 # include <Windows.h>
 #endif 
 
+//External
 #include <SDL.h>
 #include <SDL_ttf.h>
 #include <SDL_image.h>
 #include <SDL_mixer.h>
 #include <GL/glew.h>
 #include <SDL_opengl.h>
-#include <gl\glu.h>
+#include <gl/glu.h>
+#include <glm/glm.hpp>
 #include <iostream>
+
+//Internal
+#include <UIGroup.hpp>
 
 void	startGUI(SDL_Window *win);
