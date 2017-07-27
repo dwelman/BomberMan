765be214800d31452061b8e4c2659ce5d348c346
Dean du Toit <dean100@live.co.za>
2017-07-27 16:39:57 +0200

    Fixed header error

--- include/main.hpp
+++ include/main.hpp
@@ -1,18 +1,16 @@
 #pragma once
 
+//External
+#include <GL/glew.h>
 #ifdef  WIN32
 # include <Windows.h>
 #include <gl/glu.h>
 #endif 
-
-//External
 #include <SDL.h>
 #include <SDL_ttf.h>
 #include <SDL_image.h>
 #include <SDL_mixer.h>
-#include <GL/glew.h>
 #include <SDL_opengl.h>
-//#include <glu.h>
 #include <glm/glm.hpp>
 #include <iostream>
 
