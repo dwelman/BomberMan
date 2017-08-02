176f2fd035a429c54ae2f471baa9eb8e4c922603
Dean DU TOIT <ddu-toit@e4r25p3.wtc.co.za>
2017-08-02 08:16:39 +0200

    a

--- targsrc/GUIRenderer.cpp
+++ targsrc/GUIRenderer.cpp
@@ -8,10 +8,20 @@ GUIRenderer::GUIRenderer(const char * vertex_file_path, const char * fragment_fi
 {
 	shader = LoadShaders(vertex_file_path, fragment_file_path);
 	vertexBuffer = new float[vbCapacity * 18];
+	glGenBuffers(1, &VBO);
+	glGenVertexArrays(1, &VAO);
+
 };
 
 GUIRenderer::~GUIRenderer() {};
 
+void 		GUIRenderer::checkEvents(SDL_Event &e)
+{
+	for (auto it = UIElements.begin(); it != UIElements.end(); it++)
+	{
+		(*it)->checkEvent(e, true);
+	}
+}
 
 void GUIRenderer::Ortho2D(float* mat, float left, float right, float bottom, float top)
 {
@@ -50,10 +60,9 @@ void GUIRenderer::Ortho2D(float* mat, float left, float right, float bottom, flo
 void	GUIRenderer::RenderGUI()
 {
 	//Create VBO & VAO
-	glGenBuffers(1, &VBO);
+	glLoadIdentity();
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertexBufferSize, vertexBuffer, GL_DYNAMIC_DRAW);
-	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
@@ -68,8 +77,8 @@ void	GUIRenderer::RenderGUI()
 	glDrawArrays(GL_TRIANGLES, 0, 6 * UIElements.size());
 	//Cleanup
 	glDisableVertexAttribArray(0);
-	glDeleteBuffers(1, &VBO);
-	glDeleteVertexArrays(1, &VAO);
+	//glDeleteBuffers(1, &VBO);
+	//glDeleteVertexArrays(1, &VAO);
 }
 
 void	 GUIRenderer::growVB()
