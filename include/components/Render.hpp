ad75d075929b7e9d456362c219248fe8ff5b3f83
David WELMAN <daviwel@e4r25p1.wtc.co.za>
2017-08-15 09:43:00 +0200

    Reworked Render component

--- include/components/Render.hpp
+++ include/components/Render.hpp
@@ -2,29 +2,27 @@
 
 #include "Vec3.hpp"
 #include "components/BaseComponent.hpp"
+#include "main.hpp"
 #include "RenderEngine.hpp"
 #include <cstddef>
 
 class Render : public BaseComponent
 {
 private:
-    std::size_t m_meshID;
-    std::size_t m_textureID;
+    ObjectType  m_type;
     bool        m_active;
 
     Render();
 public:
-    Render(std::size_t meshID, std::size_t textureID, bool active);
-    Render(std::size_t meshID, std::size_t textureID);
+    Render(ObjectType type, bool active);
+    Render(ObjectType type);
     Render(Render const &src);
     ~Render();
 
     Render &operator=(Render const &cp);
 
-    std::size_t GetMeshID() const;
-    std::size_t GetTextureID() const;
+    ObjectType  GetObjectType() const;
     bool        GetActive() const;
-    void        SetMeshID(std::size_t meshID);
-    void        SetTextureID(std::size_t textureID);
+    void        SetObjectType(ObjectType type);
     void        SetActive(bool active);
 };
--- targsrc/components/Render.cpp
+++ targsrc/components/Render.cpp
@@ -5,17 +5,15 @@ Render::Render() : BaseComponent(RENDER)
 
 }
 
-Render::Render(std::size_t meshID, std::size_t textureID, bool active) : Render()
+Render::Render(ObjectType type, bool active) : Render()
 {
-    m_meshID = meshID;
-    m_textureID = textureID;
+    m_type = type;
     m_active = active;
 }
 
-Render::Render(std::size_t mesh, std::size_t text) : Render()
+Render::Render(ObjectType type) : Render()
 {
-    m_meshID = mesh;
-    m_textureID = text;
+    m_type = type;
     m_active = true;
 }
 
@@ -34,14 +32,9 @@ Render &Render::operator=(Render const &cp)
     return(*this);
 }
 
-std::size_t Render::GetMeshID()const
+ObjectType  Render::GetObjectType() const
 {
-    return (m_meshID);
-}
-
-std::size_t Render::GetTextureID()const
-{
-    return (m_textureID);
+    return (m_type);
 }
 
 bool Render::GetActive()const
@@ -49,14 +42,9 @@ bool Render::GetActive()const
     return (m_active);
 }
 
-void Render::SetMeshID(std::size_t meshID)
+void        Render::SetObjectType(ObjectType type)
 {
-    m_meshID = meshID;
-}
-
-void Render::SetTextureID(std::size_t textureID)
-{
-    m_textureID = textureID;
+    m_type = type;
 }
 
 void Render::SetActive(bool active)
