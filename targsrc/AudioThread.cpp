59524088dab426c4fb3dc1c1b7f37966088bcb42
Dean du Toit <dean100@live.co.za>
2017-08-17 18:30:32 +0200

    Forgot AudioThread.cpp

---
+++ targsrc/AudioThread.cpp
@@ -0,0 +1,13 @@
+#include <main.hpp>
+
+int	 AudioThread(void *ptr)
+{
+	AudioManager	*audioManager = reinterpret_cast<AudioManager*>(ptr);
+
+	do
+	{
+		//audioManager->executeQue();
+	} while (0); // Add audioManager->running() or something;
+
+	return (0);
+}
