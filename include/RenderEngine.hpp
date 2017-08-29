8ea1bd0272c879eb319d20ad2753801b07f07905
Reyno VAN DER WESTHUIZEN <reyvan-d@e4r24p2.wtc.co.za>
2017-08-29 14:52:48 +0200

    fixed bomb throbbing, added particle effects and enums

--- include/RenderEngine.hpp
+++ include/RenderEngine.hpp
@@ -9,10 +9,24 @@
 #include <glm/vec3.hpp>
 #include <glm/mat4x4.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/norm.hpp>
 
 class GameManager;
 class GameObjectRenderInfo;
 class Mesh;
+
+struct Particle {
+	glm::vec3 pos, speed;
+	unsigned char r, g, b, a;
+	float size, angle, weight;
+	float life;
+	float cameradistance;
+
+	bool operator<(const Particle& that) const {
+		// Sort in reverse order : far particles drawn first.
+		return this->cameradistance > that.cameradistance;
+	}
+};
 
 struct renderData
 {
@@ -73,6 +87,8 @@ class RenderEngine
 		GLuint loadDDS(const char * imagepath, GLuint texture);
 		bool loadOBJ(const char * path, std::vector<glm::vec3> & out_vertices, std::vector<glm::vec2> & out_uvs, std::vector<glm::vec3> & out_normals);
 		void computeTangentBasis(/*Inputs*/std::vector<glm::vec3> & vertices, std::vector<glm::vec2> & uvs, std::vector<glm::vec3> & normals, /*Outputs*/std::vector<glm::vec3> & tangents, std::vector<glm::vec3> & bitangents);
+		int FindUnusedParticle();
+		void SortParticles();
 		void initGlew();
 		int Draw(SDL_Window *window, bool gameStarted, std::vector<GameObjectRenderInfo>   gameObjects);
 
@@ -82,6 +98,9 @@ class RenderEngine
 		glm::vec3 position;
 		std::vector<renderData> rdata;
         std::vector<Mesh> mesh;
+		const int MaxParticles = 10000;
+		Particle ParticlesContainer[10000];
+		int LastUsedParticle = 0;
 
 		float horizontalAngle,
 				verticalAngle,
